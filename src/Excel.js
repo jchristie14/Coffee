22901f75731598cb2f074ddc44f9289464365a2e
John Christie <omeara.christie@gmail.com>
2016-11-07 23:45:29 -0500

    Add sort

--- src/Excel.js
+++ src/Excel.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './Excel.css';
 
 class Excel extends Component {
+  constructor(props) {
+    super(props);
+    this._sort = this._sort.bind(this);
+    this.state = {
+      data: this.props.data
+    };
+  }
+
+  _sort(e) {
+    var column = e.target.cellIndex;
+    var data = this.state.data.slice();
+    data.sort(function(a,b){
+      return a[column] > b[column] ? 1 : -1;
+    });
+    this.setState({
+      data: data,
+    });
+  }
+
   render() {
     return (
       <div className="Excel">
         <table className="table table-striped">
 
-          <thead>
+          <thead onClick={this._sort} >
             <tr>
               {
                 this.props.headers.map(function(title, idx){
@@ -20,7 +39,7 @@ class Excel extends Component {
           </thead>
 
           <tbody>
-            {this.props.data.map(function(row, rowIndex){
+            {this.state.data.map(function(row, rowIndex){
               return(
                 <tr key={rowIndex}>
                  {row.map(function(cell, cellIndex){
@@ -38,5 +57,10 @@ class Excel extends Component {
     );
   }
 }
+
+Excel.propTypes = {
+  header: React.PropTypes.arrayOf(React.PropTypes.string),
+  data: React.PropTypes.array,
+};
 
 export default Excel;
